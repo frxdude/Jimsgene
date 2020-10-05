0b99935113837466fa6c4544e4ad4e083a6202cd
tushigb <tushig.btr@gmail.com>
2020-10-06 01:13:20 +0800

    Init kk

---
+++ back-end/jimsgene/src/main/java/com/school/jimsgene/JimsgeneApplication.java
@@ -0,0 +1,13 @@
+package com.school.jimsgene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimsgeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimsgeneApplication.class, args);
+	}
+
+}
---
+++ back-end/jimsgene/src/test/java/com/school/jimsgene/JimsgeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.jimsgene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimsgeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
